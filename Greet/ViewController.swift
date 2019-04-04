c1a30971d9cc7508203df8d42d22944f8c5cf4de
sarahraines <saraines@seas.upenn.edu>
2019-04-04 18:29:24 -0400

    Initial Commit

---
+++ Greet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Greet
+//
+//  Created by Sarah Raines on 4/4/19.
+//  Copyright © 2019 Sarah Raines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
